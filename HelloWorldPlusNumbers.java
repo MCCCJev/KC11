c092052601e211f4aed029f7ff59eadd77267f62
MCCCJev <jnathaniel8518@students.mc3.edu>
2023-11-28 23:45:57 -0500

    Simple code "HelloWorld" Java class
    The class contains a main method that first prints "Hello, World!" and then proceeds to print numbers from 1 to 100 using a for loop.

---
+++ HelloWorldPlusNumbers.java
@@ -0,0 +1,10 @@
+public class HelloWorldPlusNumbers {
+    public static void main(String[] args) {
+    // Print "Hello, World!"
+        System.out.println("Hello, World!");
+          // Loop and print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
